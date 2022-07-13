a6c2e6111420a9c8952e294d2815279d017f2a84
blastxiaol <blastxiaol@gmail.com>
2022-07-13 12:51:55 +0000

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+  
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+  
+setup(
+    name="pointcloudprocess", # 用自己的名替换其中的YOUR_USERNAME_
+    version="0.0.1",    #包版本号，便于维护版本
+    author="Zhenxiang Lin",    #作者，可以写自己的姓名
+    author_email="blastxiaol@gmail.com",    #作者联系方式，可写自己的邮箱地址
+    description="A small example package",#包的简述
+    long_description=long_description,    #包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",    #自己项目地址，比如github的项目地址
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求
+)
---
+++ visualize/utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+
+def paint_points(points, color=[192,0,0]):
+    color = np.array([color])
+    new_pts = np.zeros([points.shape[0],6])
+    new_pts[:,:3] = points
+    new_pts[:, 3:] = new_pts[:, 3:] + color
+    return new_pts.astype(np.float32)
+
+def cal_corner_after_rotation(corner, center, r):
+    x1, y1 = corner
+    x0, y0 = center
+    x2 = math.cos(r) * (x1 - x0) - math.sin(r) * (y1 - y0) + x0
+    y2 = math.sin(r) * (x1 - x0) + math.cos(r) * (y1 - y0) + y0
+    return x2, y2
+
+def eight_points(center, size, rotation=0):
+    x, y, z = center
+    w, l, h = size
+    w = w/2
+    l = l/2
+    h = h/2
+
+    x1, y1, z1 = x-w, y-l, z+h
+    x2, y2, z2 = x+w, y-l, z+h
+    x3, y3, z3 = x+w, y-l, z-h
+    x4, y4, z4 = x-w, y-l, z-h
+    x5, y5, z5 = x-w, y+l, z+h
+    x6, y6, z6 = x+w, y+l, z+h
+    x7, y7, z7 = x+w, y+l, z-h
+    x8, y8, z8 = x-w, y+l, z-h
+
+    if rotation != 0:
+        x1, y1 = cal_corner_after_rotation(corner=(x1, y1), center=(x, y), r=rotation)
+        x2, y2 = cal_corner_after_rotation(corner=(x2, y2), center=(x, y), r=rotation)
+        x3, y3 = cal_corner_after_rotation(corner=(x3, y3), center=(x, y), r=rotation)
+        x4, y4 = cal_corner_after_rotation(corner=(x4, y4), center=(x, y), r=rotation)
+        x5, y5 = cal_corner_after_rotation(corner=(x5, y5), center=(x, y), r=rotation)
+        x6, y6 = cal_corner_after_rotation(corner=(x6, y6), center=(x, y), r=rotation)
+        x7, y7 = cal_corner_after_rotation(corner=(x7, y7), center=(x, y), r=rotation)
+        x8, y8 = cal_corner_after_rotation(corner=(x8, y8), center=(x, y), r=rotation)
+
+    conern1 = np.array([x1, y1, z1])
+    conern2 = np.array([x2, y2, z2])
+    conern3 = np.array([x3, y3, z3])
+    conern4 = np.array([x4, y4, z4])
+    conern5 = np.array([x5, y5, z5])
+    conern6 = np.array([x6, y6, z6])
+    conern7 = np.array([x7, y7, z7])
+    conern8 = np.array([x8, y8, z8])
+    
+    eight_corners = np.stack([conern1, conern2, conern3, conern4, conern5, conern6, conern7, conern8], axis=0)
+    return eight_corners
+
+def create_lines(cornors, color="yellow"):
+    ind = [[0, 1, 2, 3, 0], [4, 5, 6, 7, 4]]
+    vertices_list = []
+    for i in range(4):
+        vertices_list.append([cornors[ind[0][i]], cornors[ind[0][i + 1]]])
+        vertices_list.append([cornors[ind[1][i]], cornors[ind[1][i + 1]]])
+        vertices_list.append([cornors[ind[0][i]], cornors[ind[1][i]]])
+    return [{"color": color, "vertices": vertices_list[i]} for i in range(len(vertices_list))] 
